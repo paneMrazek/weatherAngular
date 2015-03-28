45b9ca389873293607e675ef4ad9eeb09362b1bf
EC2 Default User <ec2-user@ip-172-31-31-128.us-west-2.compute.internal>
2015-03-28 02:48:01 +0000

    pushing up code

---
+++ public/javascripts/ang.js
@@ -0,0 +1,118 @@
+var app = angular.module('weatherNews', ['ui.router']);
+
+app.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider){
+	$stateProvider.state('home',{
+		url:'/home',
+		templateUrl:'/home.html',
+		controller:'MainCTRL'
+	})
+	.state('posts',{
+		url:'/posts/{id}',
+		templateUrl: '/posts.html',
+		controller: 'PostCtrl'
+	});
+	$urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('MainCTRL', ['$scope', 'postFactory', function($scope, postFactory){
+	$scope.test = 'Hello World';
+	postFactory.getAll();
+	$scope.posts = postFactory.posts;
+
+	$scope.addPost = function(){
+		// $scope.posts.push({title: 'A new post', upvotes: 8});
+		// $scope.formContent = "";
+		if($scope.formContent === ""){return;}
+			postFactory.create({title:$scope.formContent,
+		});
+		$scope.title='';
+	}
+
+	$scope.incrementUpvotes=function(post){
+		postFactory.upvote(post);
+	}
+
+	$scope.decrementUpvotes = function(post){
+		postFactory.decvotes(post);
+	}
+
+}]);
+
+app.controller('PostCtrl', ['$scope','$stateParams', 'postFactory', function($scope,$stateParams, postFactory){
+	if(postFactory.posts.length == 0){
+		window.location.href="http://52.10.212.104/#/home";
+	}
+	var mypost = postFactory.posts[$stateParams.id];
+	postFactory.getPost(postFactory.posts[$stateParams.id]._id);
+	$scope.post = postFactory.post;
+
+
+	$scope.addComment = function(){
+		if($scope.body === ""){return;}
+		postFactory.addNewComment(postFactory.post._id, {
+			body: $scope.body
+		}).success(function(comment){
+			mypost.comments.push(comment);
+			postFactory.post.comments.push(comment);
+		});
+		$scope.body="";
+	};
+	$scope.incrementUpvotes = function(comment){
+		postFactory.upvoteComment(postFactory.post, comment);
+	}
+	$scope.decrementUpvotes = function(comment){
+		postFactory.downvoteComment(postFactory.post, comment);
+	}
+}]);
+
+app.factory('postFactory', ['$http',function($http){
+	var o = {
+		posts:[],
+		post:{}
+	}
+
+	o.getAll = function(){
+		return $http.get('/posts').success(function(data){
+			angular.copy(data, o.posts);
+		});
+	};
+	o.create = function(post){
+		return $http.post('/posts', post).success(function(data){
+			o.posts.push(data);
+		});
+	};
+	o.upvote = function(post){
+		return $http.put('/posts/' + post._id + '/upvote')
+			.success(function(data){
+				post.upvotes += 1;
+			});
+	};
+	o.decvotes = function(post){
+		return $http.put('/posts/' + post._id + '/upvote')
+			.success(function(data){
+				post.upvotes -= 1;
+			});
+	};
+	o.getPost = function(id){
+		return $http.get('/posts/' + id).success(function(data){
+			angular.copy(data,o.post);
+		});
+	};
+	o.addNewComment = function(id, comment){
+		return $http.post('/posts/'+ id+ '/comments', comment);
+	};
+	o.upvoteComment = function(selPost, comment){
+		return $http.put('/posts/'+ selPost._id +'/comments/'+comment._id+'/upvote')
+			.success(function(data){
+				comment.upvotes+= 1;
+			});
+	};
+	o.downvoteComment= function(selPost, comment){
+		return $http.put('/posts/'+ selPost._id +'/comments/'+comment._id+'/upvote')
+			.success(function(data){
+				comment.upvotes-= 1;
+			});
+	};
+
+	return o;
+}]);
